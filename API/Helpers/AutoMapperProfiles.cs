c10990c2f0a6e56115ab2b0ce909e8a415af704a
VasjaGit00 <vasja_vrunc@t-2.net>
2024-07-23 22:51:53 +0200

    End of Section 12

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -10,9 +10,12 @@ public class AutoMapperProfiles:Profile
    public AutoMapperProfiles()
    {
       CreateMap<AppUser,MemberDto>()
-      .ForMember(d=>d.Age,o=>o.MapFrom(s=>s.DateOfBirth.CalculateAge()))
-      .ForMember(d=>d.PhotoUrl,o=>o.MapFrom(s=>s.Photos.FirstOrDefault(x=>x.IsMain)!.Url));
+         .ForMember(d=>d.Age,o=>o.MapFrom(s=>s.DateOfBirth.CalculateAge()))
+         .ForMember(d=>d.PhotoUrl,o=>
+            o.MapFrom(s=>s.Photos.FirstOrDefault(x=>x.IsMain)!.Url));
       CreateMap<Photo,PhotoDto>();
       CreateMap<MemberUpdateDto,AppUser>();
+      CreateMap<RegisterDto,AppUser>();
+      CreateMap<string,DateOnly>().ConvertUsing(s=>DateOnly.Parse(s));
    }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,32 +1,68 @@
-import { Component, inject, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
+import { NgIf } from '@angular/common';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
 import { AccountService } from '../_services/account.service';
+import { DatePickerComponent } from "../_forms/date-picker/date-picker.component";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+    selector: 'app-register',
+    standalone: true,
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css',
+    imports: [ReactiveFormsModule,NgIf,TextInputComponent,DatePickerComponent]
 })
 
-export class RegisterComponent
+export class RegisterComponent implements OnInit
 {
    private accountService=inject(AccountService);
-   private toastr=inject(ToastrService);
+   private fb=inject(FormBuilder);
+   private router=inject(Router);
    cancelRegister=output<boolean>();
-   model:any={}
+   registerForm: FormGroup=new FormGroup({});
+   maxDate=new Date();
+   validationErrors: string[] | undefined;
 
-   register()
+   ngOnInit(): void
    {
-      this.accountService.register(this.model).subscribe({
-         next:response=>
-         {
-            console.log(response);
-            this.cancel();
-         },
-         error:error=>this.toastr.error(error.error)
+      this.initializeForm();
+      this.maxDate.setFullYear(this.maxDate.getFullYear()-18);
+   }
+
+   initializeForm()
+   {
+      this.registerForm=this.fb.group({
+         gender: ['male'],
+         username: ['', Validators.required],
+         knownAs: ['',Validators.required],
+         dateOfBirth: ['',Validators.required],
+         city: ['',Validators.required],
+         country: ['',Validators.required],
+         password: ['', [Validators.required,Validators.minLength(4),Validators.maxLength(8)]],
+         confirmPassword: ['',[Validators.required,this.matchValues('password')]],
+      });
+      this.registerForm.controls['password'].valueChanges.subscribe
+      ({
+         next: ()=>this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+      })
+   }
+
+   matchValues(matchTo: string): ValidatorFn
+   {
+      return (control: AbstractControl)=>
+      {
+         return control.value===control.parent?.get(matchTo)?.value?null:{isMatching: true}
+      }
+   }
+
+   register(): void
+   {
+      const dob=this.getDateOnly(this.registerForm.get('dateOfBirth')?.value);
+      this.registerForm.patchValue({dateOfBirth:dob});
+      this.accountService.register(this.registerForm.value).subscribe({
+         next:_=>this.router.navigateByUrl('/members'),
+         error:error=>this.validationErrors=error
       })
    }
 
@@ -34,4 +70,11 @@ export class RegisterComponent
    {
       this.cancelRegister.emit(false);
    }
+
+   private getDateOnly(dob: string | undefined)
+   {
+      if(!dob) return;
+      return new Date(dob).toISOString().slice(0,10);
+   }
+
 }
